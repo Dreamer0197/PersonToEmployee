f7a65b28261de5d4ca2a9a3e08a89223f731956e
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-17 00:27:13 +0300

    Finish

---
+++ PersonToEmployee/PersonToEmployee.h
@@ -0,0 +1,12 @@
+//
+//  PersonToEmployee.h
+//  PersonToEmployee
+//
+//  Created by Kaan Şengün on 16.11.2023.
+//
+
+#ifndef PersonToEmployee_h
+#define PersonToEmployee_h
+
+
+#endif /* PersonToEmployee_h */
