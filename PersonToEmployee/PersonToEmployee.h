61467963258793844e2accf7b2cd59367bb18efc
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-17 00:27:26 +0300

    Finish

--- PersonToEmployee/PersonToEmployee.h
+++ PersonToEmployee/PersonToEmployee.h
@@ -4,9 +4,82 @@
 //
 //  Created by Kaan Şengün on 16.11.2023.
 //
+#include <iostream>
+#include <time.h>
+using namespace std;
 
-#ifndef PersonToEmployee_h
-#define PersonToEmployee_h
-
-
-#endif /* PersonToEmployee_h */
+class Person{
+protected:
+    string personName,personSurname,personDepartment,personPosition,personID;
+    int personSalary,personYear;
+public:
+    Person(string name,string surname,string department,string position,int salary, int year){
+        personName=name; personSurname=surname; personDepartment=department; personPosition=position; personSalary=salary; personYear=year;
+    };
+    void setName(string name){personName=name;}
+    void setSurname(string surname){personSurname=surname;}
+    void setDepartment(string department){personDepartment=department;}
+    void setPosition(string position){personPosition=position;}
+    void setSalary(int salary){personSalary=salary;}
+    void setYear(int year){personYear=year;}
+    void setID(string ID){personID=ID;}
+    string getName(){return personName;}
+    string getSurname(){return personSurname;}
+    string getDepartment(){return personDepartment;}
+    string getPosition(){return personPosition;}
+    string getID(){return personID;}
+    int getSalary(){return personSalary;}
+    int getYear(){return personYear;}
+    void print(){
+        cout<<"-------------------------------------"<<endl;
+        cout<<"Name:"<<personName<<" "<<personSurname<<endl;
+        cout<<"Department:"<<personDepartment<<endl;
+        cout<<"Position:"<<personPosition<<endl;
+        cout<<"Years Worked:"<<personYear<<endl;
+        cout<<"ID Number:"<<personID<<endl;
+    }
+};
+class Employee:public Person{
+private:
+    int employeeBonus;
+public:
+    Employee(string name,string surname,string department,string position,int salary, int year):Person(name, surname,department,position,salary,year){
+        employeeBonus=85*year;
+        int randomID=rand()%8999+1000;
+        Employee::setID("EMP"+ to_string(randomID));
+    }
+    Employee() : Person("", "", "", "", 0, 0) {
+            employeeBonus = 0;
+    }
+    void print(){
+        Person::print();
+        cout<<"Bonus:"<<employeeBonus<<endl;
+        cout<<"------------------------"<<endl;
+    }
+};
+class Manager:public Employee{
+private:
+    int managerBonus;
+public:
+    Manager(string name,string surname,string department,string position,int salary, int year):Employee(name,surname,department,position,salary,year){
+        managerBonus=150*year;
+        int randomID=rand()%8999+1000;
+        Manager::setID("MNG"+to_string(randomID));
+        
+    }
+    void print(){
+        Person::print();
+        cout<<"Bonus:"<<managerBonus<<endl;
+        cout<<"------------------------"<<endl;
+    }
+    void extractEmployees(Employee employeesArray[],int arraySize){
+        cout<<"Manager Name:"<<Manager::getName()<<" "<<Manager::getSurname()<<endl;
+        cout<<"Manager ID:"<<Manager::getID()<<endl;
+        cout<<"\n"<<getDepartment()<<" Department Employee List"<<endl;
+        for(int i=0;i<arraySize;i++){
+            if(getDepartment()==employeesArray[i].getDepartment()){
+                employeesArray[i].print();
+            }
+        }
+    }
+};
--- PersonToEmployee/main.cpp
+++ PersonToEmployee/main.cpp
@@ -6,9 +6,23 @@
 //
 
 #include <iostream>
+#include "PersonToEmployee.h"
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    const int numEmployees = 10;
+    Employee employees[numEmployees];
+    
+    Employee employee1={"Ogun","Birinci","Software","Engineer",4500,5};
+    Employee employee2={"Eda","Deniz","Software","Engineer",8500,5};
+    Employee employee3={"Burak","Ikinci","IT","Programmer",4500,5};
+    Employee employee4={"Ogun","Birinci","Accounting","President",4500,5};
+    Manager manager1("Duygu","Keydal","Software","Manager",8500,10);
+    Manager manager2("Omer","Selvi","IT","Manager",15000,25);
+    
+    Employee employeeList[]={employee1,employee2,employee3,employee4};
+    manager1.extractEmployees(employeeList,numEmployees);
+    manager1.print();
+    
     return 0;
 }
